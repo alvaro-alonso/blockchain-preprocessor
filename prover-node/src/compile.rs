759edeaf15a4c841ef0806c28818f42a6a255c28
alvaro-alonso <alfmail@pm.me>
2022-07-28 12:27:48 +0200

    #4 write compile outputs to fs

--- prover-node/src/compile.rs
+++ prover-node/src/compile.rs
@@ -2,15 +2,16 @@ use rocket::response::status::NotFound;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::fs::{relative};
 use serde_json::to_writer_pretty;
-use std::fs::{File, create_dir, write};
+use std::fs::{File, create_dir, write, remove_dir_all};
 use std::io::BufWriter;
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 use typed_arena::Arena;
 use sha2::{Sha256, Digest};
 use zokrates_core::compile::{compile, CompileConfig, CompileError};
 use zokrates_core::typed_absy::abi::Abi;
 use zokrates_field::{Bn128Field, Field};
 use zokrates_fs_resolver::FileSystemResolver;
+
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -29,24 +30,75 @@ pub struct CompileResponseBody {
 pub fn post_compile_zokrates(
     req_body: Json<CompileRequestBody>,
 ) -> Result<Json<CompileResponseBody>, NotFound<String>> {
-    // FIXME: remove clone for static pointer
-    match api_compile::<Bn128Field>(req_body.program.clone()) {
-        Ok(resp) => Ok(Json(resp)),
-        Err(str) => Err(NotFound(str)),
-    }
-}
-
-fn api_compile< T: Field>(code: String) -> Result<CompileResponseBody, String> {
-    let hash = format!("{:X}", Sha256::digest(&code));
+    // create a hash for the .zok code, if the hash exists return err
+    let program = req_body.program.clone();
+    let hash = format!("{:X}", Sha256::digest(&program));
     let path = Path::new(relative!("out")).join(&hash);
     if path.is_dir() {
-        return Err(String::from("proof already exists"))
+        return Err(NotFound(String::from("proof already exists")))
     } 
-    
+
+    // create all file paths
     let program_path = path.join("program.zok");
     let bin_output_path = path.join("out");
     let abi_spec_path = path.join("abi.json");
-    let program = code.to_string();
+    let arena = Arena::new();
+
+    // compile .zok code
+    let (program_flattened, abi) =  api_compile::<Bn128Field>(&program, &program_path, &arena)
+        .map_err(|e| NotFound(e))?;
+        
+    // if compilation successful write .zok, binary and abi file under the hash folder
+    let write_outputs = || -> Result<usize, String> {
+        // create dir with the hash of the program
+        create_dir(&path).map_err(|e| e.to_string())?;
+
+        // serialize flattened program and write to binary file
+        log::debug!("Serialize program");
+        let bin_output_file = File::create(&bin_output_path)
+            .map_err(|why| format!("Could not create {}: {}", bin_output_path.display(), why))?;
+        let mut writer = BufWriter::new(bin_output_file);
+        let constrain_count = program_flattened.serialize(&mut writer)
+            .map_err(|e| e.to_string())?;
+
+        // serialize ABI spec and write to JSON file
+        log::debug!("Serialize ABI");
+        let abi_spec_file = File::create(&abi_spec_path)
+            .map_err(|why| format!("Could not create {}: {}", abi_spec_path.display(), why))?;
+        let mut writer = BufWriter::new(abi_spec_file);
+        to_writer_pretty(&mut writer, &abi)
+            .map_err(|_| "Unable to write data to file.".to_string())?;
+
+        // write .zok file in folder
+        write(&program_path, &program).expect("Unable to write .zok file");
+    
+        Ok(constrain_count)
+    };
+    
+    match write_outputs() {
+        Ok(constrain_count) => {
+            log::info!("zokrates program written to '{}'", program_path.display());
+            log::info!("Compiled code written to '{}'", bin_output_path.display());
+            log::info!("abi file written to '{}'", abi_spec_path.display());
+            log::info!("Number of constraints: {}", constrain_count);
+            Ok(Json(CompileResponseBody {
+                proof_id: hash,
+                abi,
+            }))
+        },
+        Err(e) => {
+            // something wrong happened, clean up
+            remove_dir_all(path).unwrap();
+            Err(NotFound(e.to_string()))
+        },
+    }
+}
+
+fn api_compile<'a, T: Field>(code: &'a str, program_path: &'a PathBuf, arena: &'a Arena<String>) 
+    -> Result<(
+        zokrates_core::ir::ProgIterator<T, impl std::iter::IntoIterator<Item = zokrates_core::ir::Statement<T>> + 'a>,
+        zokrates_core::typed_absy::abi::Abi
+    ), String> {
 
     let fmt_error = |e: &CompileError| {
         let file = e.file().canonicalize().unwrap();
@@ -71,54 +123,18 @@ fn api_compile< T: Field>(code: String) -> Result<CompileResponseBody, String> {
     let config = CompileConfig::default();
     let resolver = FileSystemResolver::with_stdlib_root(stdlib_path);
     log::debug!("Compile");
-    let arena = Arena::new();
-    let artifacts =
-        compile::<T, _>(program.clone(), program_path.clone(), Some(&resolver), config, &arena).map_err(|e| {
-            format!(
-                "Compilation failed:\n\n{}",
-                e.0.iter()
-                    .map(|e| fmt_error(e))
-                    .collect::<Vec<_>>()
-                    .join("\n\n")
-            )
-        })?;
-
-    let (program_flattened, abi) = artifacts.into_inner();
-    // after successful compilation create proof folder and write all outputs
-    create_dir(&path).map_err(|e| e.to_string())?;
-    // serialize flattened program and write to binary file
-    log::debug!("Serialize program");
-    let bin_output_file = File::create(&bin_output_path)
-        .map_err(|why| format!("Could not create {}: {}", bin_output_path.display(), why))?;
-    let mut writer = BufWriter::new(bin_output_file);
-
-    match program_flattened.serialize(&mut writer) {
-        Ok(constraint_count) => {
-        
-            write(&program_path, program).expect("Unable to write .zok file");
-            // serialize ABI spec and write to JSON file
-            log::debug!("Serialize ABI");
-            let abi_spec_file = File::create(&abi_spec_path)
-                .map_err(|why| format!("Could not create {}: {}", abi_spec_path.display(), why))?;
-            let mut writer = BufWriter::new(abi_spec_file);
-            to_writer_pretty(&mut writer, &abi)
-                .map_err(|_| "Unable to write data to file.".to_string())?;
-
-            log::info!("zokrates program written to '{}'", program_path.display());
-            log::info!("Compiled code written to '{}'", bin_output_path.display());
-            log::info!("abi file written to '{}'", abi_spec_path.display());
-            log::info!("Number of constraints: {}", constraint_count);
-            Ok(CompileResponseBody {
-                proof_id: hash,
-                abi,
-            })
-        }
-        Err(e) => {
-            // something wrong happened, clean up
-            std::fs::remove_file(bin_output_path).unwrap();
-            Err(e.to_string())
-        }
-    }
+    
+    let program = code.to_string();
+    match compile::<T, _>(program.clone(), program_path.clone(), Some(&resolver), config, &arena) {
+        Ok(artifacts) => Ok(artifacts.into_inner()),
+        Err(e) => Err(format!(
+            "Compilation failed:\n\n{}",
+            e.0.iter()
+                .map(|e| fmt_error(e))
+                .collect::<Vec<_>>()
+                .join("\n\n")
+        )),
+    }    
 }
 
 // FIXME: add unittest for route
