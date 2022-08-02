17bf934f08fa5a6ba72139d67e6392140ab9e430
alvaro-alonso <alfmail@pm.me>
2022-08-02 16:37:12 +0200

    #4 api responses refactor

--- prover-node/src/utils/responses.rs
+++ prover-node/src/utils/responses.rs
@@ -1,8 +1,25 @@
-use rocket::http::Status;
-use rocket::{response, response::Responder};
-use rocket::request::Request;
 use rocket::serde::{json::Json, Serialize};
+use rocket::{response, response::{Response, Responder}};
+use rocket::http::{ContentType, Status};
+use rocket::request::Request;
 
+
+pub type ApiResult<T> = Result<ApiResponse<T>, ApiError>;
+
+#[derive(Debug)]
+pub struct ApiResponse<T> {
+    pub response: T,
+    pub status: Status,
+}
+
+impl<'r, 'o: 'r, T: Serialize> Responder<'r, 'o> for ApiResponse<T> {
+    fn respond_to(self, req: &'r Request<'_>) -> response::Result<'o> {
+        Response::build_from(Json(self.response).respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
 
 #[derive(Debug)]
 pub enum ApiError {
@@ -17,9 +34,6 @@ pub enum ApiError {
 pub struct ErrorResponse {
     pub error_message: String,
 }
-
-pub type ApiResult<T> = Result<Json<T>, ApiError>;
-
 
 impl<'r, 'o: 'r> Responder<'r, 'o> for ApiError {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'o> {
