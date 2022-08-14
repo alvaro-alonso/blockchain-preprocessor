b172ff5c4f349a4528d7affbe6b6816cd2f5a089
alvaro-alonso <alfmail@pm.me>
2022-08-14 18:55:40 +0200

    #4 add health route

---
+++ prover-node/src/routes/health.rs
@@ -0,0 +1,33 @@
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket_okapi::{openapi, JsonSchema};
+
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(crate = "rocket::serde")]
+pub struct Task {
+    service_name: String,
+    version: String,
+}
+
+#[openapi]
+#[get("/health", format = "json")]
+pub fn health() -> Json<Task> {
+    Json(Task {
+        // TODO: add version to response
+        service_name: "Zokrates Prover".to_string(),
+        version: "0.1".to_string(),
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use rocket::http::{ContentType, Status};
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn json_test_index() {
+        let client = Client::tracked(super::rocket()).unwrap();
+        let res = client.get("/health").header(ContentType::JSON).dispatch();
+        assert_eq!(res.status(), Status::Ok);
+    }
+}
